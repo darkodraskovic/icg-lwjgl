86e09d0dbb69c2f315961fe79f3a7f9b1ae551f3
Darko Draskovic <darko.draskovic@gmail.com>
2020-03-09 19:21:46 +0100

    Add genArrayBufferf to Mesh
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- src/org/lwjglb/engine/graph/Mesh.java
+++ src/org/lwjglb/engine/graph/Mesh.java
@@ -1,18 +1,78 @@
 package org.lwjglb.engine.graph;
 
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBindTexture;
 import static org.lwjgl.opengl.GL11.glDrawArrays;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
 import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
 import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
 import static org.lwjgl.opengl.GL15.glDeleteBuffers;
+import static org.lwjgl.opengl.GL15.glGenBuffers;
 import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 import static org.lwjgl.opengl.GL30.glBindVertexArray;
 import static org.lwjgl.opengl.GL30.glDeleteVertexArrays;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.lwjgl.system.MemoryUtil;
 
 abstract public class Mesh {
-	protected int vao;
-	protected int[] vbo;
+	private int vao;
+	private ArrayList<Integer> vbos;
+
+	public Mesh() {
+		vao = glGenVertexArrays();
+		vbos = new ArrayList<Integer>();
+	}
+
+	protected void genArrayBufferf(float[] vertices, int index, int size) {
+		int vbo;
+		FloatBuffer buffer = null;
+
+		try {
+			buffer = MemoryUtil.memAllocFloat(vertices.length);
+			buffer.put(vertices).flip();
+
+			glBindVertexArray(vao);
+
+			// Create the VBO and bind to it
+			vbo = glGenBuffers();
+			glBindBuffer(GL_ARRAY_BUFFER, vbo);
+			glBufferData(GL_ARRAY_BUFFER, buffer, GL_STATIC_DRAW);
+			// Enable location 0
+			glEnableVertexAttribArray(index);
+			// Define structure of the data
+			glVertexAttribPointer(index, size, GL_FLOAT, false, 0, 0);
+
+			// Unbind the VBO
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+			// Unbind the VAO
+			glBindVertexArray(0);
+		} finally {
+			if (buffer != null) {
+				MemoryUtil.memFree(buffer);
+			}
+		}
+
+		vbos.add(vbo);
+	}
 
 	abstract public void draw();
+
+	protected void setTexture2D(int texture, int target) {
+		glActiveTexture(texture);
+		glBindTexture(GL_TEXTURE_2D, target);
+	}
 
 	protected void drawArrays(int mode, int first, int count) {
 		// Bind to the VAO
@@ -25,13 +85,25 @@ abstract public class Mesh {
 		glBindVertexArray(0);
 	}
 
-	public void cleanup() {		
+	protected void drawElements(int mode, int count, int type) {
+		// Bind to the VAO
+		glBindVertexArray(vao);
+
+		// Draw the vertices
+		glDrawElements(mode, count, type, 0);
+
+		// Restore state
+		glBindVertexArray(0);
+	}
+
+	public void cleanup() {
 		glDisableVertexAttribArray(0);
 
 		// Delete the VBO
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		for (int i = 0; i < vbo.length; i++) {
-			glDeleteBuffers(vbo[i]);
+		for (Iterator<Integer> iterator = vbos.iterator(); iterator.hasNext();) {
+			Integer integer = (Integer) iterator.next();
+			glDeleteBuffers(integer);
 		}
 
 		// Delete the VAO
--- src/org/lwjglb/game/Triangle.java
+++ src/org/lwjglb/game/Triangle.java
@@ -1,71 +1,17 @@
 package org.lwjglb.game;
 
-import static org.lwjgl.opengl.GL11.GL_FLOAT;
 import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
-import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
-import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
-import static org.lwjgl.opengl.GL15.glBindBuffer;
-import static org.lwjgl.opengl.GL15.glBufferData;
-import static org.lwjgl.opengl.GL15.glGenBuffers;
-import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
-import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
-import static org.lwjgl.opengl.GL30.glBindVertexArray;
-import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
-import java.nio.FloatBuffer;
-
-import org.lwjgl.system.MemoryUtil;
 import org.lwjglb.engine.graph.Mesh;
 
 public class Triangle extends Mesh {
-	
+
 	public Triangle() {
 		float[] vertices = new float[] { 0.0f, 0.5f, -1.0f, -0.5f, -0.5f, -1.0f, 0.5f, -0.5f, -1.0f };
 		float[] colors = new float[] { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
 
-		FloatBuffer verticesBuffer = null;
-		FloatBuffer colorsBuffer = null;
-		
-		try {
-			// Create the VAO and bind to it
-			vao = glGenVertexArrays();
-			glBindVertexArray(vao);
-
-			// Create the VBO array
-			vbo = new int[2];
-
-			verticesBuffer = MemoryUtil.memAllocFloat(vertices.length);
-			verticesBuffer.put(vertices).flip();
-			// Create the VBO and bind to it
-			vbo[0] = glGenBuffers();
-			glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-			glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
-			// Enable location 0
-			glEnableVertexAttribArray(0);
-			// Define structure of the data
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-
-			colorsBuffer = MemoryUtil.memAllocFloat(colors.length);
-			colorsBuffer.put(colors).flip();
-			// Create the VBO and bind to it
-			vbo[1] = glGenBuffers();
-			glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-			glBufferData(GL_ARRAY_BUFFER, colorsBuffer, GL_STATIC_DRAW);
-			// Enable location 1
-			glEnableVertexAttribArray(1);
-			// Define structure of the data
-            glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
-            
-			// Unbind the VBO
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-			// Unbind the VAO
-			glBindVertexArray(0);
-		} finally {
-			if (verticesBuffer != null) {
-				MemoryUtil.memFree(verticesBuffer);
-			}
-		}
+		genArrayBufferf(vertices, 0, 3);
+		genArrayBufferf(colors, 1, 3);
 	}
 
 	@Override
